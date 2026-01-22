8fa79199a18ff95726030db3172c66ea2a5a5ef1
Mahmoud <you@example.com>
2026-01-22 12:19:00 +0200

    add correct order function

--- main.js
+++ main.js
@@ -1,26 +1,49 @@
-document.querySelector('.controls-btn span').addEventListener('click', () => {
+// document.querySelector('.controls-btn span').addEventListener('click', () => {
 
-    let name = prompt("ادخل اسمك");
+//     let name = prompt("ادخل اسمك");
 
-    if (name === null || name === '') {
-        document.querySelector('.name span').innerHTML = 'لم يتم ادخال اسم';
-    } else {
-        document.querySelector('.name span').innerHTML = name;
+//     if (name === null || name === '') {
+//         document.querySelector('.name span').innerHTML = 'لم يتم ادخال اسم';
+//     } else {
+//         document.querySelector('.name span').innerHTML = name;
 
-    }
-    document.querySelector('.controls-btn').remove()
+//     }
+//     document.querySelector('.controls-btn').remove()
   
-})
+// })
+
+// unOrderImages
 
 let unOrderImages = document.querySelector('.unOrder-images')
 let images = Array.from(unOrderImages.children)
 let reorderImage = [...Array(images.length).keys()]
+
+let currentStep = 0;
+
 
 reorderImage = shuffle(reorderImage)
 
 images.forEach((image , index)=>{
 
     image.style.order = reorderImage[index]
+
+    image.addEventListener('click',()=>{
+
+        // console.log(image.dataset.ablution);
+
+        if(image.dataset.ablution === correctReorderArray[currentStep]){
+
+            console.log('good');
+
+            currentStep++
+        }
+
+        else{
+
+            console.log('bad');
+        }
+    })
+
 })
 
 function shuffle(array){
@@ -45,3 +68,13 @@ function shuffle(array){
         }
         return array;
 }
+
+let correctReorderArray =  ["intention","rinsing","inhalation","Washing-the-face","wash-hands-elbows","head-wipe","ear-wiping","washing-feet"];
+
+// order-images
+
+let orderImagesContainer = document.querySelector(".order-images")
+let orderImages = Array.from(orderImagesContainer.children)
+let correctReorderImages = [...Array(orderImages.length).keys()]
+
+
